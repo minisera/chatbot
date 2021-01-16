5ae26476403ad8b912ddd9e16390aab305e26e25
minisera <koki.ohtaka0821@gmail.com>
2021-01-16 11:24:52 +0900

    feat: 質問するとチャットが下にスクロールするよう、App.jsx変更

--- src/App.jsx
+++ src/App.jsx
@@ -36,6 +36,13 @@ export default class App extends React.Component {
       case (nextQuestionId === "init"):
         this.displayNextQuestion(nextQuestionId)
         break;
+        
+      case (/^https:*/.test(nextQuestionId)):
+        const a = document.createElement("a");
+        a.href = nextQuestionId;
+        a.target = '_blank';
+        a.click();
+        break;
       default:
         const chats = this.state.chats;
         chats.push({
@@ -47,7 +54,9 @@ export default class App extends React.Component {
           chats: chats
         })
 
-        this.displayNextQuestion(nextQuestionId)
+        setTimeout(()=> {
+          this.displayNextQuestion(nextQuestionId)
+        },500)
 
         break;
     }
@@ -56,6 +65,13 @@ export default class App extends React.Component {
   componentDidMount() {
     const initAnswer = "";
     this.selectAnswer(initAnswer, this.state.currentId)
+  }
+
+  componentDidUpdate() {
+    const scrollArea = document.getElementById("scroll-area")
+    if (scrollArea) {
+      scrollArea.scrollTop = scrollArea.scrollHeight;
+    }
   }
 
   render() {
