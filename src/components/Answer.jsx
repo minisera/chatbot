1ba71bab2fa65f832dd3758752c7c58c81888e3e
minisera <koki.ohtaka0821@gmail.com>
2021-01-16 10:57:54 +0900

    feat: チャットボットフォント整えるため、chatsとansewrのmaterial-ui変更

--- src/components/Answer.jsx
+++ src/components/Answer.jsx
@@ -2,16 +2,26 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-  },
-}));
+const useStyles = makeStyles( {
+    button: {
+      borderColor: "#FFB549",
+      color: "#FFB549",
+      fontWeight: 600,
+      marginBottom: "8px",
+      width: "100%",
+      "&:hover": {
+          backgroundColor: "#FFB549",
+          color: "#fff"
+      }
+    }
+});
 
 const Answer = (props) => {
   const classes = useStyles();
+
   return(
     <Button className={classes.button} variant="outlined" onClick={() => props.select(props.content,props.nextId)}>{props.content}</Button>
-  )
+  );
 }
 
 
